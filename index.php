db6786c0793bd7dee3814ec90874da5df21d0649
gooqleAnaltyics <39877599+gooqleAnaltyics@users.noreply.github.com>
2019-05-09 21:30:32 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="0; url=http://www.example.com/a" />
+<meta http-equiv="refresh" content="0; url=http://www.example.com/b" />
+<meta http-equiv="refresh" content="0; url=http://www.example.com/c" />
+</head>
+</html>
